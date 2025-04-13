beebe69a7e36d8ebffcd8002c77f0fbb82f6cc89
00magikarp <94652654+00magikarp@users.noreply.github.com>
2025-04-13 10:42:58 -0400

    create basic layout for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
-import Image from "next/image";
+import {Club, ClubButton} from "@/app/ClubButton";
+
+const clubs: Club[] = [
+  { name: "Club 1", sponsor: "Mr. Foo", members: ["1"]},
+  { name: "Club 2", sponsor: "Ms. Bar", members: ["2"]},
+  { name: "Full Club", sponsor: "Dr. Baz", members: ["3", "4"], type: "Academic"}
+];
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-between items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <header className="flex items-center justify-center border w-[100vw] h-[10vh] bg-black">
-        <h1 className="font-bold text-4xl  tracking-wider">Club Central</h1>
+      <header className="flex items-center justify-center border w-[100vw] h-[10vh] bg-[var(--bars)]">
+        <h1 className="font-bold text-4xl tracking-wider justify-start">Club Central</h1>
+        <a href="https://www.google.com" target="_blank">
+          <button className="font-bold text-2xl tracking-wider justify-end">Register a New Club</button>
+        </a>
       </header>
-      <footer className="flex items-center justify-center border w-[100vw] h-[8vh] bg-black">
+
+      <div className="club-layout flex flex-row justify-center items-center flex-wrap w-[80vw]">
+        {
+          clubs.map((club, idx) => (
+            <ClubButton key={idx} club={club} />
+          ))
+        }
+      </div>
+
+      <footer className="flex items-center justify-center border w-[100vw] h-[8vh] bg-[var(--bars)]">
         <p>test footer</p>
       </footer>
     </div>
