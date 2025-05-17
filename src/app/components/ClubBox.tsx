428037c671ddff639055379b44779ede68f10764
00magikarp <94652654+00magikarp@users.noreply.github.com>
2025-05-16 21:01:49 -0400

    remove info button and make the whole thing a modalbutton

--- src/app/components/ClubBox.tsx
+++ src/app/components/ClubBox.tsx
@@ -8,49 +8,49 @@ type ClubBoxProps = {
 
 export function ClubBox({ club }: ClubBoxProps) {
   return (
-    <div className="p-3 flex flex-col m-3 max-w-[380px] min-w-[200px] h-[140px] text-xl bg-[var(--mid)] text-gray rounded-md select-text items-center justify-start">
-      <div className="w-full flex flex-col justify-start items-center mr-auto mb-auto mt-1">
-        <h3 className="font-semibold text-lg">{ club.name }</h3>
-        <p className="text-gray-400 text-lg">{ club.type || "" }</p>
-      </div>
-      <div className="w-full justify-center items-center max-w-[150px]">
-        <ModalButton
-          buttonClass="w-full h-[35px] bg-[var(--fssgold)] rounded-sm justify-center items-center
-          cursor-pointer hover:bg-neutral-900 hover:text-[var(--fssgold)] mb-[10px]
-          hover:shadow-lg hover:scale-[1.02] custom-button"
-          modalClass="text-gray-300"
-          buttonTitle="Info"
-          modalTitle={ club.name }
-          modalBody={
-            <div className="rounded">
-              <p className="text-gray-400 text-lg text-center">{club.type || "Other"}</p>
-              <br/>
-              <p>{club.description || "No description found"}</p>
-              <br/>
-              <p>Student(s) in charge:</p>
-              <ul className="list-disc list-inside">
-                {
-                  club.student_leads_name.map((student: string, idx: number) => (
-                    <li key={idx}>{ club.student_leads_name[idx] } (<a className="underline underline-offset-4" href={"mailto:" + club.student_leads_contact[idx]}>{ club.student_leads_contact[idx] }</a>)</li>
-                  ))
-                }
-              </ul>
-              <br/>
-              <p>Sponsor(s):</p>
-              <ul className="list-disc list-inside">
-                {
-                  club.sponsors_name.map((student: string, idx: number) => (
-                    <li key={idx}>{ club.sponsors_name[idx]} (<a className="underline underline-offset-4" href={"mailto:" + club.sponsors_contact[idx]}>{ club.sponsors_contact[idx] }</a>)</li>
-                  ))
-                }
-              </ul>
-              <br/>
-              <p>Meets @ <b>{club.time || "unstructured times"}</b> in <b>{club.location || "different places"}</b></p>
-              <p>Additional information: { club.other || "None" }</p>
-            </div>
-          }
-        />
-      </div>
+    <div>
+      <ModalButton
+        buttonClass="
+        p-3 flex flex-col m-3 max-w-[380px] min-w-[200px] h-[120px] text-xl bg-[var(--mid)] text-gray rounded-md select-text items-center justify-start
+        transform transition-transform duration-200 hover:scale-105
+        "
+        modalClass="text-gray-300"
+        buttonTitle={
+        <div className="h-full w-full flex flex-col justify-start items-center mt-2 mt-1">
+          <h3 className="h-[50%] font-semibold text-xl">{club.name}</h3>
+          <p className="text-gray-400 text-lg">{club.type || ""}</p>
+        </div>
+        }
+        modalTitle={ club.name }
+        modalBody={
+          <div className="rounded">
+            <p className="text-gray-400 text-lg text-center">{club.type || "Other"}</p>
+            <br/>
+            <p>{club.description || "No description found"}</p>
+            <br/>
+            <p>Student(s) in charge:</p>
+            <ul className="list-disc list-inside">
+              {
+                club.student_leads_name.map((student: string, idx: number) => (
+                  <li key={idx}>{ club.student_leads_name[idx] } (<a className="underline underline-offset-4" href={"mailto:" + club.student_leads_contact[idx]}>{ club.student_leads_contact[idx] }</a>)</li>
+                ))
+              }
+            </ul>
+            <br/>
+            <p>Sponsor(s):</p>
+            <ul className="list-disc list-inside">
+              {
+                club.sponsors_name.map((student: string, idx: number) => (
+                  <li key={idx}>{ club.sponsors_name[idx]} (<a className="underline underline-offset-4" href={"mailto:" + club.sponsors_contact[idx]}>{ club.sponsors_contact[idx] }</a>)</li>
+                ))
+              }
+            </ul>
+            <br/>
+            <p>Meets @ <b>{club.time || "unstructured times"}</b> in <b>{club.location || "different places"}</b></p>
+            <p>Additional information: { club.other || "None" }</p>
+          </div>
+        }
+      />
     </div>
   )
 }
